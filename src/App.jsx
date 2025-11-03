2a611f9492acb3f41b62bfbfe8ff311076dfd258
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-11-03 16:22:00 +0530

    Add movie loading Default spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Search from "./components/search";
+import Spinner from "./components/Spinner";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -17,6 +18,7 @@ const App = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  console.log(movieList);
 
   const fetchMovies = async () => {
     setIsLoading(true);
@@ -60,15 +62,17 @@ const App = () => {
             <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
           </header>
           <section className="all-movies">
-            <h2>All Movies</h2>
+            <h2 className="mt-[40px]">All Movies</h2>
             {isLoading ? (
-              <p className="text-white">Loading...</p>
+              <Spinner />
             ) : errorMessage ? (
               <p className="text-red-500">{errorMessage}</p>
             ) : (
               <ul>
                 {movieList.map((movie) => (
-                  <p className="text-white">{movie.title}</p>
+                  <p key={movie.id} className="text-white">
+                    {movie.title}
+                  </p>
                 ))}
               </ul>
             )}
